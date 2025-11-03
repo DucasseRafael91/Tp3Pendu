0cde605a26774eecf115a11feff9e03decc5f17e
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-03 13:02:49 +0100

    Ajout des mots à deviner

--- src/tp3Pendu/Tp3Pendu.java
+++ src/tp3Pendu/Tp3Pendu.java
@@ -28,20 +28,6 @@ public class Tp3Pendu {
             "été", 
             "mer"
         };
-
-        int tailleTableau = 10; // nombre de mots à générer
-        String[] motsAleatoires = new String[tailleTableau];
-        Random random = new Random();
-
-        for (int i = 0; i < tailleTableau; i++) {
-            int index = random.nextInt(Words.length);
-            motsAleatoires[i] = Words[index];
-        }
-
-        System.out.println("Tableau de mots aléatoires :");
-        for (String mot : motsAleatoires) {
-            System.out.print(mot + " ");
-        }
     }
 
 }
